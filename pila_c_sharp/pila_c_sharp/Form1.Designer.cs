acdd35dd9d9ece4aaab34b52bf730768c830cd6f
SergioPosse <sergiodavidposse@gmail.com>
2020-02-09 14:37:35 -0300

    Initial Commit

---
+++ pila_c_sharp/pila_c_sharp/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace pila_c_sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_top = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_tamaño = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "PUSH (apilar)";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button2.Location = new System.Drawing.Point(12, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 52);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "POP";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button3.Location = new System.Drawing.Point(12, 151);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 48);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "PEEK";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 37;
+            this.listBox1.Location = new System.Drawing.Point(164, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 185);
+            this.listBox1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(303, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "TOP";
+            // 
+            // lbl_top
+            // 
+            this.lbl_top.AutoSize = true;
+            this.lbl_top.Location = new System.Drawing.Point(303, 30);
+            this.lbl_top.Name = "lbl_top";
+            this.lbl_top.Size = new System.Drawing.Size(33, 13);
+            this.lbl_top.TabIndex = 10;
+            this.lbl_top.Text = "vacio";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(303, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "TAMAÑO";
+            // 
+            // lbl_tamaño
+            // 
+            this.lbl_tamaño.AutoSize = true;
+            this.lbl_tamaño.Location = new System.Drawing.Point(305, 119);
+            this.lbl_tamaño.Name = "lbl_tamaño";
+            this.lbl_tamaño.Size = new System.Drawing.Size(13, 13);
+            this.lbl_tamaño.TabIndex = 12;
+            this.lbl_tamaño.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(366, 210);
+            this.Controls.Add(this.lbl_tamaño);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbl_top);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_top;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_tamaño;
+    }
+}
+
---
+++ pila_c_sharp/pila_c_sharp/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pila_c_sharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        //VARIABLES GLOBALES PARA QUE ESTEN DISPONIBLES EN TODAS LAS CLASES
+        public static class glo
+        {
+            public static int cont = 0;
+            public static Stack<int> aux = new Stack<int>();
+        }
+        //FUNCIONA PARA CONTROLAR SI LA PILA ESTA VACIA
+        public bool  pila_vacia() 
+        {
+            if (glo.aux.Count <= 0)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)//BOTON PARA HACER PUSH
+        {
+            glo.aux.Push(glo.cont); //PUSH A LA PILA GLOBAL UN VALOR CUALQUIERA, PODRIA SER UN RANDOM
+                                    //PERO ME ERA MAS FACIL CONTROLAR CON NUMEROS CONSECUTIVOS.
+
+
+            int i = new int(); //DECLARO i PARA USAR EN EL FOR QUE ME RENDERIZA LA LISTA AL REVEZ
+
+            listBox1.Items.Clear(); //LIMPIO LA LISTA
+
+            for (i = 0; i < glo.aux.Count(); i++)
+            {
+                listBox1.Items.Add(Convert.ToString(glo.aux.ElementAt(i)));
+            }
+           
+            //EL CONTADOR SOLO LO USO PARA CARGAR UN VALOR COMO PRUEBA
+            glo.cont = glo.cont + 1;
+            //ACTUALIZO SIEMPRE EL TOP DE LA PILA EN UN LABEL
+            lbl_top.Text = (Convert.ToString(glo.aux.Peek()));
+            //actualizo el label para el tamaño de la pila
+            lbl_tamaño.Text = Convert.ToString(glo.aux.Count());
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) //BOTON PARA HACER POP
+        {
+            //SI LA PILA ESTA VACIA ENTONCES MUESTRA MENSAJE 
+            if (pila_vacia())
+            {
+                MessageBox.Show("Pila vacia");
+            }
+            else //SINO HACE EL POP A LA PILA
+            {
+                glo.aux.Pop();
+
+
+                //AHORA RENDERIZO DE NUEVO LA LISTA PARA QUE SE MUESTRE AL REVEZ
+                int i = new int();
+
+                listBox1.Items.Clear();
+
+                for (i = 0; i < glo.aux.Count(); i++)
+                {
+                    listBox1.Items.Add(glo.aux.ElementAt(i));
+                }
+                if (pila_vacia())
+                {
+                    lbl_top.Text = "vacia";
+                }
+                else
+                {
+                    lbl_top.Text = (Convert.ToString(glo.aux.Peek()));
+                }
+                //actualizo el label para el tamaño de la pila
+                lbl_tamaño.Text=Convert.ToString(glo.aux.Count());
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)// BOTON PARA HACER PEEK
+        {
+            if (pila_vacia())
+            {
+                MessageBox.Show("Pila vacia");
+            }
+            else
+            {
+                MessageBox.Show(Convert.ToString(glo.aux.Peek()));
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
